42fe3097dd2965f39e3df14d4ad6a60d3bd019ba
kamla <kbharia502@gmail.com>
2024-11-21 19:03:04 +0530

    21december

---
+++ main.js
@@ -0,0 +1,72 @@
+console.log("hello");
+
+let a = 45;
+let b =50;
+let c = a+b;
+console.log(c);
+
+var d = 45;
+var e =50;
+var f = d+e;
+console.log(f);
+
+
+const g = 45;
+const h =50;
+const i = g+h;
+console.log(i);
+
+let j = 45;
+var k =50;
+const l = j+k;
+console.log(l);
+
+
+let age = 21;
+if(age>= 18){
+    console.log("You are eligible for vote")
+}
+else{
+    console.log("Not eligible")
+}
+
+let sym = Symbol('unique');
+console.log(typeof(sym));
+
+
+let num = null;
+console.log(typeof(num));
+let sum ;
+console.log(typeof(sum));
+  
+// for loop
+
+let hey = "hello";
+
+for(let i = 1; i<=10; i++){
+    console.log(hey);
+}
+
+// do While loop
+
+let num1 = 1 ;
+ do{
+    console.log(num1);
+    num1++;
+ }while(num1<=50);
+
+// while loop
+
+let num2 =0 ;
+while(num2<=10){
+    console.log(num2);
+    num2 = num2+2;
+}
+
+let tab = 20;
+for (let i = 20; i <= 40; i += 2) {
+    console.log(i);
+}
+
+
+
---
+++ nov21.js
@@ -0,0 +1,11 @@
+// 1. Write a program to print all natural numbers from 1 to n. – using while
+// loop
+
+
+let n = 10; 
+let i = 1; 
+
+while (i <= n) {
+    console.log(i); 
+    i++; 
+}
