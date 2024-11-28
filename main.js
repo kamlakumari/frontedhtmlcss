c5a076899a032048bd365404d7918ecf294d4936
kamla <kbharia502@gmail.com>
2024-11-28 13:27:42 +0530

    array of object

--- main.js
+++ main.js
@@ -205,88 +205,88 @@
 // let arr = [...array11 , ...array22];
 // console.log(arr);
 
-arr = [5,5,6,2,1]
+// arr = [5,5,6,2,1]
 
-for(i=0; i<arr.length; i++){
-   console.log(arr[i+1]);
-}
+// for(i=0; i<arr.length; i++){
+//    console.log(arr[i+1]);
+// }
 
 
-Arr2 = [2,3,4,7,6]; 
-let target = 7;
-for(i=0; i<Arr2.length; i++){
-   if(Arr2[i]===target){
-      console.log(`Matched ${target}` )
-   }
-}
+// Arr2 = [2,3,4,7,6]; 
+// let target = 7;
+// for(i=0; i<Arr2.length; i++){
+//    if(Arr2[i]===target){
+//       console.log(`Matched ${target}` )
+//    }
+// }
 
 
 // find sum of inner array fom the main array value/
 
-   let Arr1 =[5,6,4,8,[4,6,20,15,45,88,96,44],9,3,2,1]
-let sum = 0;
-   for(i in Arr1){
-      if(Array.isArray(Arr1[i])){
-         console.log(Arr1[i]);
-       for(j of Arr1[i]){
-         sum = sum + j;
-       }
-      }
+//    let Arr1 =[5,6,4,8,[4,6,20,15,45,88,96,44],9,3,2,1]
+// let sum = 0;
+//    for(i in Arr1){
+//       if(Array.isArray(Arr1[i])){
+//          console.log(Arr1[i]);
+//        for(j of Arr1[i]){
+//          sum = sum + j;
+//        }
+//       }
      
-   }
-   console.log(Array.isArray(Arr1))
-   console.log(sum)
+//    }
+//    console.log(Array.isArray(Arr1))
+//    console.log(sum)
 
 
    // find the squre root of element and find the string in the array and make the string in the upper case
 
-   Arrar1 = [2,1,3,[12, 21, 11, 10], "kamla", "Jaipur"]
-  let sum1 = 0;
-   for(a in Arrar1[i]){
-      if(typeof a=== "number"){
-  console.log(a*a);
-      }
-      else if(Array.isArray(a)){
-         console.log("Areay is found");
-         for(i of a){
-            sum1 = sum1+i
-         }
+//    Arrar1 = [2,1,3,[12, 21, 11, 10], "kamla", "Jaipur"]
+//   let sum1 = 0;
+//    for(a in Arrar1[i]){
+//       if(typeof a=== "number"){
+//   console.log(a*a);
+//       }
+//       else if(Array.isArray(a)){
+//          console.log("Areay is found");
+//          for(i of a){
+//             sum1 = sum1+i
+//          }
          
-      }
-      else{
-         console.log(a.toString)
-      }
-   }
-console.log(sum1)
+//       }
+//       else{
+//          console.log(a.toString)
+//       }
+//    }
+// console.log(sum1)
 
 
 
-let user ={
-   firstname:"sachin",
-   lastName: "Thalor",
-   age: 20,
-   address:{
-       city:"jaipur",
-       state:"rajasthan",
-       country:"india"
-   },
+// let user ={
+//    firstname:"sachin",
+//    lastName: "Thalor",
+//    age: 20,
+//    address:{
+//        city:"jaipur",
+//        state:"rajasthan",
+//        country:"india"
+//    },
  
-   hobbies:["cricket","football" ,["chess", "sudoko"],"reading"],
-   isMarried:false 
+//    hobbies:["cricket","football" ,["chess", "sudoko"],"reading"],
+//    isMarried:false 
        
-}
+// }
 
-console.log(user.hobbies[0])
+// console.log(user.hobbies[0])
 
-if(user.hobbies[0]==="cricket"){
-   console.log("play")
-}
-else{
-   console.log("not play")
-}
-console.log(user.hobbies.includes("cricket"));
+// if(user.hobbies[0]==="cricket"){
+//    console.log("play")
+// }
+// else{
+//    console.log("not play")
+// }
+// console.log(user.hobbies.includes("cricket"));
 
-console.log(user.hobbies[2].includes("chess"));
+// console.log(user.hobbies[2].includes("chess"));
 
 // let {age, gender, hobbies} = user
 // console.log(gender)
@@ -294,114 +294,218 @@ console.log(user.hobbies[2].includes("chess"));
 
 // destrucring
 
-let { firstname:fn , age:a , grnder } = user
-console.log(fn, a)
+// let { firstname:fn , age:a , grnder } = user
+// console.log(fn, a)
     
 
   
-let data =  [{
-   "id": 1,
-   "first_name": "Annissa",
-   "last_name": "Rubinov",
-   "email": "arubinov0@prweb.com",
-   "gender": "Female",
-   "ip_address": "4.25.213.14",
-   "salery": 10000,
- }, {
-   "id": 2,
-   "first_name": "Sarena",
-   "last_name": "Gut",
-   "email": "sgut1@usda.gov",
-   "gender": "Female",
-   "ip_address": "165.183.145.15",
-   "salery": 50000,
- }, {
-   "id": 3,
-   "first_name": "Jacquette",
-   "last_name": "Leal",
-   "email": "jleal2@cmu.edu",
-   "gender": "Female",
-   "ip_address": "136.79.88.6",
-   "salery": 82000,
- }, {
-   "id": 4,
-   "first_name": "Fawne",
-   "last_name": "Purkins",
-   "email": "fpurkins3@free.fr",
-   "gender": "Female",
-   "ip_address": "41.96.173.217",
-   "salery": 88000,
- }, {
-   "id": 5,
-   "first_name": "Zacharia",
-   "last_name": "Elvins",
-   "email": "zelvins4@yandex.ru",
-   "gender": "Male",
-   "ip_address": "12.218.236.92",
-   "salery": 85000,
- }, {
-   "id": 6,
-   "first_name": "Karim",
-   "last_name": "Harpur",
-   "email": "kharpur5@slashdot.org",
-   "gender": "Male",
-   "ip_address": "39.140.108.163",
-   "salery": 60000,
- }, {
-   "id": 7,
-   "first_name": "Doyle",
-   "last_name": "Batham",
-   "email": "dbatham6@cdbaby.com",
-   "gender": "Male",
-   "ip_address": "146.186.250.17",
-   "salery": 70000,
- }, {
-   "id": 8,
-   "first_name": "Base",
-   "last_name": "Lakey",
-   "email": "blakey7@hubpages.com",
-   "gender": "Male",
-   "ip_address": "249.124.77.126",
-   "salery": 90000,
- }, {
-   "id": 9,
-   "first_name": "Jeremiah",
-   "last_name": "Champley",
-   "email": "jchampley8@samsung.com",
-   "gender": "Male",
-   "ip_address": "64.143.25.17",
-   "salery": 80000,
+// let data =  [{
+//    "id": 1,
+//    "first_name": "Annissa",
+//    "last_name": "Rubinov",
+//    "email": "arubinov0@prweb.com",
+//    "gender": "Female",
+//    "ip_address": "4.25.213.14",
+//    "salery": 10000,
+//  }, {
+//    "id": 2,
+//    "first_name": "Sarena",
+//    "last_name": "Gut",
+//    "email": "sgut1@usda.gov",
+//    "gender": "Female",
+//    "ip_address": "165.183.145.15",
+//    "salery": 50000,
+//  }, {
+//    "id": 3,
+//    "first_name": "Jacquette",
+//    "last_name": "Leal",
+//    "email": "jleal2@cmu.edu",
+//    "gender": "Female",
+//    "ip_address": "136.79.88.6",
+//    "salery": 82000,
+//  }, {
+//    "id": 4,
+//    "first_name": "Fawne",
+//    "last_name": "Purkins",
+//    "email": "fpurkins3@free.fr",
+//    "gender": "Female",
+//    "ip_address": "41.96.173.217",
+//    "salery": 88000,
+//  }, {
+//    "id": 5,
+//    "first_name": "Zacharia",
+//    "last_name": "Elvins",
+//    "email": "zelvins4@yandex.ru",
+//    "gender": "Male",
+//    "ip_address": "12.218.236.92",
+//    "salery": 85000,
+//  }, {
+//    "id": 6,
+//    "first_name": "Karim",
+//    "last_name": "Harpur",
+//    "email": "kharpur5@slashdot.org",
+//    "gender": "Male",
+//    "ip_address": "39.140.108.163",
+//    "salery": 60000,
+//  }, {
+//    "id": 7,
+//    "first_name": "Doyle",
+//    "last_name": "Batham",
+//    "email": "dbatham6@cdbaby.com",
+//    "gender": "Male",
+//    "ip_address": "146.186.250.17",
+//    "salery": 70000,
+//  }, {
+//    "id": 8,
+//    "first_name": "Base",
+//    "last_name": "Lakey",
+//    "email": "blakey7@hubpages.com",
+//    "gender": "Male",
+//    "ip_address": "249.124.77.126",
+//    "salery": 90000,
+//  }, {
+//    "id": 9,
+//    "first_name": "Jeremiah",
+//    "last_name": "Champley",
+//    "email": "jchampley8@samsung.com",
+//    "gender": "Male",
+//    "ip_address": "64.143.25.17",
+//    "salery": 80000,
    
    
- }]
+//  }]
 
- console.log(data[0])
- console.log(data[8].email)
-let totalsal = 0
- for(i of data){
-   // console.log(i.email)
-   // console.log(i.salery)
- totalsal = totalsal + i.salery
- }
- console.log(totalsal)
+//  console.log(data[0])
+//  console.log(data[8].email)
+// let totalsal = 0
+//  for(i of data){
+//    // console.log(i.email)
+//    // console.log(i.salery)
+//  totalsal = totalsal + i.salery
+//  }
+//  console.log(totalsal)
    
- let totalmale = 0;
- let totalfemale = 0;
- for(i of data){
-   if(i.gender==="Male"){
-      totalmale++;
-   }
-   else if(i.gender==="Female"){
-      totalfemale++;
-   }
- }
- console.log(totalmale);
- console.log(totalfemale);
+//  let totalmale = 0;
+//  let totalfemale = 0;
+//  for(i of data){
+//    if(i.gender==="Male"){
+//       totalmale++;
+//    }
+//    else if(i.gender==="Female"){
+//       totalfemale++;
+//    }
+//  }
+//  console.log(totalmale);
+//  console.log(totalfemale);
 
- let emp = 0;
- for(i of data){
-   if(i.salery>=50000){
-      emp++;
+//  let emp = 0;
+//  for(i of data){
+//    if(i.salery>=50000){
+//       emp++;
+//    }
+//  }
+// console.log(`number of employe which salary above to 50000  = ${emp}`)
+
+
+// practice Questions
+
+// --
+// Filter users who are aged 25 or younger.
+// Map the array to get an array of user names.
+// Filter orders with an amount greater than 50.
+// Map the array to get an array of order IDs.
+// Filter users who have at least one order with a status of 'pending'.
+// Map the array to get an array of objects containing user names and their total order amounts.
+// Filter orders with an amount between 30 and 70.
+// Map the array to get an array of objects containing user names and their completed order IDs.
+// Filter users who live in the 'USA' and have at least one order with an amount greater than 50.
+// Filter users who live in the 'USA' and have at least one order with an amount greater than 50
+
+
+const complexData = [
+   {
+     id: 1,
+     name: "John Doe",
+     age: 30,
+     address: {
+       street: "123 Main St",
+       city: "Anytown",
+       country: "USA",
+     },
+     orders: [
+       { orderId: "A123", amount: 50.75, status: "completed" },
+       { orderId: "B456", amount: 30.25, status: "pending" },
+     ],
+   },
+   {
+     id: 2,
+     name: "Jane Smith",
+     age: 25,
+     address: {
+       street: "456 Oak St",
+       city: "Othercity",
+       country: "USA",
+     },
+     orders: [
+       { orderId: "C789", amount: 100.5, status: "completed" },
+       { orderId: "D012", amount: 75.2, status: "completed" },
+     ],
+   },
+ ]
+
+
+ // / Filter users who are aged 25 or younger.
+ let user = 0;
+ for(u of complexData){
+   if(u.age>25){
+      user++
    }
  }
-console.log(`number of employe which salary above to 50000  = ${emp}`)
+ console.log(`User who is Younger than 25 ${user}`)
+
+//  -----------------------------------------------------------------------
+
+// / Map the array to get an array of user names.
+
+ for(u of complexData){
+   console.log(u.name)
+ }
+
+
+//  ------------------------------------------------------------------------
+// Filter orders with an amount greater than 50.
+ 
+
+
+for(u of complexData){
+   if(u.address.country == 'USA'){
+     
+     if(u.orders[0].amount || u.orders[1].amount > 50){
+      console.log(u)
+     }
+   }
+ }
+
+//  console.log(u)                 //????
+
+
+//  -------------------------------------------------------------------
+// Map the array to get an array of order IDs.
+
+for(d of complexData){
+   console.log(`the id of complexData ${d.id}`)
+}
+
+// ---------------------------------------------------------------------
+// Filter users who have at least one order with a status of 'pending'.
+ 
+for(data of complexData){
+      for(a of data.orders){
+         if(a.status == 'pending'){
+            console.log(a)
+         }
+      }
+}  
+// -------------------------------------------------------------------
+
+
