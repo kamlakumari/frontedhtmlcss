d63c5361f319558af632b7f0ae6a8c767b5b771e
kamla <kbharia502@gmail.com>
2025-01-07 13:22:19 +0530

    arr

--- main.js
+++ main.js
@@ -1719,36 +1719,36 @@
 // console.log(res)
 
    
-const complexData = [
-   {
-     id: 1,
-     name: "John Doe",
-     age: 30,
-     address: {
-       street: "123 Main St",
-       city: "Anytown",
-       country: "USA",
-     },
-     orders: [
-       { orderId: "A123", amount: 50.75, status: "completed" },
-       { orderId: "B456", amount: 30.25, status: "pending" },
-     ],
-   },
-   {
-     id: 2,
-     name: "Jane Smith",
-     age: 25,
-     address: {
-       street: "456 Oak St",
-       city: "Othercity",
-       country: "USA",
-     },
-     orders: [
-       { orderId: "C789", amount: 100.5, status: "completed" },
-       { orderId: "D012", amount: 75.2, status: "completed" },
-     ],
-   },
- ]
+// const complexData = [
+//    {
+//      id: 1,
+//      name: "John Doe",
+//      age: 30,
+//      address: {
+//        street: "123 Main St",
+//        city: "Anytown",
+//        country: "USA",
+//      },
+//      orders: [
+//        { orderId: "A123", amount: 50.75, status: "completed" },
+//        { orderId: "B456", amount: 30.25, status: "pending" },
+//      ],
+//    },
+//    {
+//      id: 2,
+//      name: "Jane Smith",
+//      age: 25,
+//      address: {
+//        street: "456 Oak St",
+//        city: "Othercity",
+//        country: "USA",
+//      },
+//      orders: [
+//        { orderId: "C789", amount: 100.5, status: "completed" },
+//        { orderId: "D012", amount: 75.2, status: "completed" },
+//      ],
+//    },
+//  ]
 
 // 1... Filter users who are aged 25 or younger.
 
@@ -1772,5 +1772,34 @@ const complexData = [
 // })
 
 // 3...Filter orders with an amount greater than 50.
+
+
+// ================================  07-1-2025---- ++++++++++++++++++++++++++++++
+
+let arr = [[1,2,3],[4,5,6],[7,8,9]]
+
+// let b = arr.reduce((a,  c)=>{
+//   return  [...a, ...c]
+// })
+// console.log(b)
+
+// ------------------------------------
+
+// let b = arr.reduce((a,  c)=>{
+//     return a.concat(c)
+//   },[])
+//   console.log(b)
+// -------------------------------------------
+
+
+// for(let a of arr){
+  
+//   console.log(a)
+// }
+
+// let a = arr.map((c)=>{
+//   return c
+// })
+// console.log(a)
 
 
